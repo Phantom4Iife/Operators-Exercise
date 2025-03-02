528c60e1eb6357edaba3eae069f198740d90ff55
Jeremiah Augustin <Phantom4iife7@gmail.com>
2025-03-02 13:06:02 -0500

    Finished Project

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,33 @@
 ﻿namespace OperatorExercise
 {
-    public class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            int quotient = a / b; 
+            int remainder = a % b; 
+            
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            
+            double area = AreaOfCircle(radius);
+            
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
+            
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            static double AreaOfCircle(double radius)
+            {
+                return Math.PI * Math.Pow(radius, 2);
+            }
         }
     }
-}
+}    
